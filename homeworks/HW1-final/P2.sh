bc89bd8c9e023f8e2ac469f46eb40f4652b0896b
Chris Siviy <csiviy@g.harvard.edu>
2019-09-12 10:10:55 -0400

    Removed extra lines to make it four lines only

--- homeworks/HW1-final/P2.sh
+++ homeworks/HW1-final/P2.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 
-# # 0. Word count
-# cat apollo13.txt | wc
-
-# A. Check how many lines have numbers
-echo "A:"
 grep -E "[0-9]" apollo13.txt | wc -l
 
-# B. -c option in `grep`
-echo "\nB:"
 man grep | col -b | grep -A 1 "^ *-c" | tr "\n" " "
 
-# C. .py files
 find test-dir -regex ".*\.\py" | wc -l
 
-# D. Files without "others" read/write access
 find . -maxdepth 1 -type f ! -perm -o=rw | wc -l
