f7db0a8c398c907fa66c4908a9350ad18b62b335
Chris Siviy <csiviy@g.harvard.edu>
2019-09-12 10:11:51 -0400

    Removed reference to test-dir

--- homeworks/HW1-final/P2.sh
+++ homeworks/HW1-final/P2.sh
@@ -4,6 +4,6 @@ grep -E "[0-9]" apollo13.txt | wc -l
 
 man grep | col -b | grep -A 1 "^ *-c" | tr "\n" " "
 
-find test-dir -regex ".*\.\py" | wc -l
+find . -regex ".*\.\py" | wc -l
 
 find . -maxdepth 1 -type f ! -perm -o=rw | wc -l
